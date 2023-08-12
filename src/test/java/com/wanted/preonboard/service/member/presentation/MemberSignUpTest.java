0f59d7d2d185defa22b4561a9b0cadc19bbb7f2d
JinuCheon <woojin8787@gmail.com>
2023-08-12 22:02:48 +0900

    1. MemberSignUpTest 테스트 생성

---
+++ src/test/java/com/wanted/preonboard/service/member/presentation/MemberSignUpTest.java
@@ -0,0 +1,12 @@
+package com.wanted.preonboard.service.member.presentation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberSignUpTest {
+
+    @Test
+    void signUp() {
+    }
+}
