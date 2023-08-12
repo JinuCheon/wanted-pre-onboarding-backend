f2b106106c71e5170cf911df8c81b9546cb47989
JinuCheon <woojin8787@gmail.com>
2023-08-12 20:52:46 +0900

    init: 스프링 부트 초기 설정

---
+++ src/main/java/com/wanted/preonboard/PreonboardApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.preonboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreonboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreonboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/preonboard/PreonboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.preonboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreonboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
