11c1f227b622190b207ff4a926d9a4bce02523d2
JinuCheon <woojin8787@gmail.com>
2023-08-13 06:01:06 +0900

    9. JPA 적용

--- src/main/java/com/wanted/preonboard/member/domain/Member.java
+++ src/main/java/com/wanted/preonboard/member/domain/Member.java
@@ -1,9 +1,34 @@
 package com.wanted.preonboard.member.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Entity
+@Table(name = "member")
+@Comment("회원")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
-    private final String nickname;
-    private final String email;
-    private final String password;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
 
     public Member(final String nickname, final String email, final String password) {
         this.nickname = nickname;
--- src/main/java/com/wanted/preonboard/member/domain/MemberRepository.java
+++ src/main/java/com/wanted/preonboard/member/domain/MemberRepository.java
@@ -1,21 +1,6 @@
 package com.wanted.preonboard.member.domain;
 
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.HashMap;
-import java.util.List;
-
-@Component
-public class MemberRepository {
-    private static final HashMap<Long, Member> members = new HashMap<>();
-    private static Long sequence = 0L;
-
-    public List<Member> findAll() {
-        return List.copyOf(members.values());
-    }
-
-    public Member save(final Member member) {
-        members.put(sequence++, member);
-        return member;
-    }
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
