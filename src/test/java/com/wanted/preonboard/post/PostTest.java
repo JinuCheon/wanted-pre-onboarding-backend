d69de4f6b7bad1bc0c1b2b0c6301c036bff3bfcc
JinuCheon <woojin8787@gmail.com>
2023-08-14 04:39:09 +0900

    3. 포스트 생성 요청을 위한 CreatePostRequest 정의

--- src/test/java/com/wanted/preonboard/post/PostTest.java
+++ src/test/java/com/wanted/preonboard/post/PostTest.java
@@ -2,6 +2,7 @@ package com.wanted.preonboard.post;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +20,11 @@ class PostTest {
 
     @Test
     void createPost() {
+        final CreatePostRequest request = new CreatePostRequest(
+                "title",
+                "content"
+        );
+        postService.createPost(request);
         assertThat(postRepository.findAll()).hasSize(1);
     }
 
@@ -30,5 +36,12 @@ class PostTest {
     }
 
     private class Post {
+    }
+
+    public record CreatePostRequest(String title, String content) {
+        public CreatePostRequest {
+            Assert.hasText(title, "title must not be empty");
+            Assert.hasText(content, "content must not be empty");
+        }
     }
 }
