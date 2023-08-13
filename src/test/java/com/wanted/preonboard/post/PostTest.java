8c2c5b5449a9dd3d29f6deb0730bff1c0d6355cb
JinuCheon <woojin8787@gmail.com>
2023-08-14 04:35:05 +0900

    2. POJO PostRepository 정의

--- src/test/java/com/wanted/preonboard/post/PostTest.java
+++ src/test/java/com/wanted/preonboard/post/PostTest.java
@@ -1,11 +1,34 @@
 package com.wanted.preonboard.post;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class PostTest {
 
-    @Test
-    void createPost() {
+    private PostRepository postRepository;
+
+    @BeforeEach
+    void setUp() {
+        postRepository = new PostRepository();
     }
 
+    @Test
+    void createPost() {
+        assertThat(postRepository.findAll()).hasSize(1);
+    }
+
+    private class PostRepository {
+        HashMap<Long, Post> posts = new HashMap<>();
+        public List<Post> findAll() {
+            return List.copyOf(posts.values());
+        }
+    }
+
+    private class Post {
+    }
 }
